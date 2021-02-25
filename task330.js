072aa5e49e30be92296be09b2c1df82256ba7f5f
dmytro07 <dmytro.furtas07@gmail.com>
2021-02-25 23:35:05 +0200

    Completed task 330

---
+++ task330.js
@@ -0,0 +1,27 @@
+/*
+ * Натуральное число называется совершенным, если оно
+ * равно сумме всех своих делителей, за исключением себя самого. Число
+ * 6 – совершенное, так как 6 = 1+2+3. Число 8 – не совершенное, так как
+ * 8 ≠ 1+2+4.Дано натуральное число n. Получить все совершенные числа,
+ * меньшие n.
+ */
+
+function getPerfectNumbers(n) {
+  const result = [];
+  for (let i = 6; i < n; i++) {
+    if (isPerfectNumber(i)) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+function isPerfectNumber(num) {
+  let sumOfDivisors = 1;
+  for (let i = 2, half = Math.floor(num / 2); i <= half; i++) {
+    if (num % i === 0) {
+      sumOfDivisors += i;
+    }
+  }
+  return num === sumOfDivisors;
+}
