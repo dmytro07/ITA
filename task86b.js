010c5556698b18c71ad48b278d13b0baf4d1488d
dmytro07 <dmytro.furtas07@gmail.com>
2021-02-25 22:59:06 +0200

    Completed task 86b

---
+++ task86b.js
@@ -0,0 +1,12 @@
+/* Дано натуральное число n.
+ * б) Чему равна сумма его цифр?
+ */
+
+function getSumOfDigits(n) {
+  let counter = 0;
+  while (n !== 0) {
+    counter += n % 10;
+    n = Math.floor(n / 10);
+  }
+  return counter;
+}
